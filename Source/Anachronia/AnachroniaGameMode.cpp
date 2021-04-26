007b0cc44af16084d3f1fc21a68eb80ee8d6017c
Benjamin <mafortion.benjamin@gmail.com>
2021-04-26 11:32:40 +0200

    Fix missing sound effects in builds by forcing FMOD to initalize first in gamemode constructor.

--- Source/Anachronia/AnachroniaGameMode.cpp
+++ Source/Anachronia/AnachroniaGameMode.cpp
@@ -3,11 +3,17 @@
 #include "AnachroniaGameMode.h"
 #include "AnachroniaHUD.h"
 #include "AnachroniaCharacter.h"
+#include "FMODStudioModule.h"
 #include "UObject/ConstructorHelpers.h"
 
 AAnachroniaGameMode::AAnachroniaGameMode()
 	: Super()
 {
+	// Force FMOD to initialize before loading player pawn.
+	// Otherwise FMOD events referenced by Anachronia Player will just silently fail to load
+	// See: https://qa.fmod.com/t/cannot-play-fmod-events-in-package-build/12248/3
+	IFMODStudioModule::Get();
+	
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/AnachroniaPlayerBP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
