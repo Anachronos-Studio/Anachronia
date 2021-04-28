6a9a6e26018cf2bb9d1d668c52023d1ad8fa4f95
Benjamin <mafortion.benjamin@gmail.com>
2021-04-28 14:10:00 +0200

    Hide guard direction arrow when bDebugInfo is off

--- Source/Anachronia/AI/GuardPawn.cpp
+++ Source/Anachronia/AI/GuardPawn.cpp
@@ -68,10 +68,15 @@ void AGuardPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector EyesLoc;
-	FRotator EyesRot;
-	GetActorEyesViewPoint(EyesLoc, EyesRot);
-	DrawDebugDirectionalArrow(GetWorld(), EyesLoc, EyesLoc + EyesRot.Vector() * 100.0f, 10.0f, FColor::White, false, -1, 0, 1.0f);
+#if !UE_BUILD_SHIPPING
+	if (bDebugInfo)
+	{
+		FVector EyesLoc;
+		FRotator EyesRot;
+		GetActorEyesViewPoint(EyesLoc, EyesRot);
+		DrawDebugDirectionalArrow(GetWorld(), EyesLoc, EyesLoc + EyesRot.Vector() * 100.0f, 10.0f, FColor::White, false, -1, 0, 1.0f);
+	}
+#endif
 }
 
 AGuardAIController* AGuardPawn::GetGuardAI() const
